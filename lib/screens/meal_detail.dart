3e30cfc120e504d9e0c0f3c1adc450a9c32a67a1
Avatbar <tadeasmachacek@gmail.com>
2023-04-28 16:44:15 +0200

    added favorite meals marker

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -11,6 +11,9 @@ class MealDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -25,7 +28,7 @@ class MealDetailScreen extends ConsumerWidget {
                 content: Text(wasAdded ? "Meal added as a favorite." : "Meal removed."),
               ));
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           )
         ],
       ),
